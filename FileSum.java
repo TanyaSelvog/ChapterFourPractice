8a0b5d8ffb1fb0355c47a019a1e6721fc09d5883
Tanya <TanyaSelvog@gmail.com>
2021-07-29 14:59:17 -0700

    added FileSum
    from 4.11

---
+++ FileSum.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.io.*;
+
+/**
+This program reads a series of numbers from a file
+and accumulates their sum.
+*/
+
+public class FileSum
+{
+    public static void main(String[] args) throws IOException
+    {
+        double sum = 0.0; // accumulator, initialized to 0
+
+        //Open the file for reading.
+        File file = new File("Numbers.txt");
+        Scanner inputFile = new Scanner(file);
+
+        // Read all of the values fromo the file & calculate total
+        while (inputFile.hasNext())
+        {
+            // Read a value from the file
+            double number = inputFile.nextDouble();
+
+            // Add the number to sum
+            sum = sum + number;
+        }
+
+        // Close the file
+        inputFile.close();
+
+        // Display the sum of the numbers
+        System.out.println("The sum of the numbers in " +
+        "Numbers.txt is " + sum);
+    }
+}
